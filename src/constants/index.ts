cb986d067118ab2739b9f2a3b52a681feada685e
andrew <andrew.c.tran1@gmail.com>
2024-05-31 21:30:34 -0400

    Reorganize boilerplate directory structure and remove unused files

--- src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-import constants from './constants';
-import theme, {COLORS, SIZES, FONTS} from './theme';
-
-export {constants, theme, COLORS, SIZES, FONTS};
--- src/utils/format.tsx
+++
@@ -1,1 +0,0 @@
-export const getCurrentDateString = () => new Date(Date.now()).toISOString();
